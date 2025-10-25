f1165a891b0e4fcbacd84c9048efd722d7abd862
Ilya Avtonomov <laymed@mail.ru>
2025-10-25 11:49:40 +0300

    Лабораторная работа №1

---
+++ src/LAB1algorithms/Palindrome.java
@@ -0,0 +1,17 @@
+package LAB1algorithms;
+
+public class Palindrome {
+
+    public static String reverseString(String s) {
+        String reversed = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed = reversed + s.charAt(i);
+        }
+        return reversed;
+    }
+
+    public static boolean isPalindrome(String s) {
+        String reversed = reverseString(s);
+        return s.equals(reversed);
+    }
+}
---
+++ src/LAB1algorithms/Primes.java
@@ -0,0 +1,13 @@
+package LAB1algorithms;
+
+public class Primes {
+
+    public static boolean isPrime(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
